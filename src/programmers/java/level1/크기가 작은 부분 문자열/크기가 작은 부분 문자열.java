968888244756c579a66d698ff18b93645eb3fbc1
junhyeonpark <j0303p@gmail.com>
2025-04-25 20:06:26 +0900

    level1 : 크기가 작은 부분 문자열

---
+++ src/programmers/java/level1/크기가 작은 부분 문자열/크기가 작은 부분 문자열.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String t, String p) {
+        int pLength = p.length();
+        long pValue = Long.parseLong(p);
+        int answer = 0;
+        for (int i = 0; i <= t.length() - pLength; i++) {
+            long tValue = Long.parseLong(t.substring(i, i + pLength));
+            if (tValue <= pValue)
+                answer++;
+        }
+        return answer;
+    }
+}
