3becf68e5321e5a59e5e56cac716f1dc54b8cb53
junhyeonpark <j0303p@gmail.com>
2025-05-21 20:22:41 +0900

    스티커 모으기(2).java

---
+++ src/programmers/java/level3/스티커 모으기(2) /스티커 모으기(2).java
@@ -0,0 +1,28 @@
+class Solution {
+    int max=0;
+    public int solution(int sticker[]) {
+        int n = sticker.length;
+
+        int []dp1 = new int [n];
+        int []dp2 = new int [n];
+
+        if(n==1)
+            return sticker[0];
+
+
+        dp1[0]=sticker[0];
+        dp1[1]=sticker[0];
+
+        for(int i=2;i<n-1;i++){
+            dp1[i]=Math.max(dp1[i-1],dp1[i-2]+sticker[i]);
+        }
+
+
+        dp2[0]=0;
+        dp2[1]=sticker[1];
+        for(int i=2;i<n;i++){
+            dp2[i]=Math.max(dp2[i-1],dp2[i-2]+sticker[i]);
+        }
+        return Math.max(dp1[n-2],dp2[n-1]);
+    }
+}
